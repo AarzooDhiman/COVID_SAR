f436cdd41aa2ee086a772af99f5cb87f75133874
AarzooDhiman <34305704+AarzooDhiman@users.noreply.github.com>
2023-06-19 09:00:26 +0100

    Update ct-bert2-crossval.py

--- ct-bert2-crossval.py
+++ ct-bert2-crossval.py
@@ -78,46 +78,24 @@ def read_pickle1(pth):
 
 def datasplit(label, feat):
     
-    #df = read_pickle1('/disks/sdb/adhiman/SAR-z/labelbox itr3/with consensus/data_for_validation/itr123_train.pkl')
-    df = read_pickle1('/disks/sdb/adhiman/SAR-z/labelbox itr3/with consensus/data_for_validation/itr123.pkl')
+    
+    df = read_pickle1('labeled_tweet.pkl')
     df['ABT_FAMILY'] = df['FAMILY'].astype(bool)
     df = clean_tweets(df,False, remove_duplicates=False)
     
     
-    '''df1 = read_pickle1('/home/adhiman/SAR-z/to_label_data/code/final_labels/labeled_SET1.pkl')
-    df2 = read_pickle1('/disks/sdb/adhiman/SAR-z/labelbox itr2/with_consensus/itr2.pkl')
-    df2 = df2[list(df1.columns.values)]
-    df2 = df2.replace('NO', 0)
-    df2 = df2.replace('YES', 1)
-    df = pd.concat([df1,df2])
-    '''
+    
 
-    '''df = read_pickle1('/home/adhiman/SAR-z/labeled data/itr1_2.pkl')
     
-    #print (df.shape)
-    #print (df.columns.values)
-    #df_test = pd.read_csv('/disks/sdb/adhiman/SAR-z/labelbox itr3/without consesus/matching/data_itr3_mtch.csv')
-    df_test = pd.read_csv('/disks/sdb/adhiman/SAR-z/labelbox itr3/with consensus/itr3.csv')
-    
-    df_test = df_test.replace('NO', 0).replace('YES', 1)
-    df_test=df_test[['TWEET_ID', 'USER_ID', 'TIMESTAMP' ,'TWEET_TEXT' ,'AUTHOR_COVID', 'AUTHOR_SYMPTOMS' ,'FAMILY', 'FAMILY_COVID', 'FAMILY_SYMPTOMS', 'TWEET_TEXT_PROCESSED']]
-    df_test = clean_tweets(df_test,False, remove_duplicates=False)
-    print (df_test.shape)
-    df = pd.concat([df, df_test])
-    
-    
-    #df = read_pickle1('/home/adhiman/SAR-z/Eda/processed/new/auged_1_2.pkl')
-
-    df =df.sample(frac=1)
-    print (df.shape)
-    print (df.head())
 
 
     df["AUTHOR_OR"] = df["AUTHOR_COVID"] + df["AUTHOR_SYMPTOMS"] > 0
     df["FAMILY_OR"] = df["FAMILY_COVID"] + df["FAMILY_SYMPTOMS"] > 0
 
     df["AUTHOR_OR"] = df["AUTHOR_OR"].replace(0, False).replace(1,True)
-    df["FAMILY_OR"] = df["FAMILY_OR"].replace(0, False).replace(1,True)'''
+    df["FAMILY_OR"] = df["FAMILY_OR"].replace(0, False).replace(1,True)
+
+   
     
     
     label = label
@@ -134,21 +112,6 @@ def datasplit(label, feat):
 
 
 
-
-    '''class_size = df[label].sum()
-    df_new = pd.concat([
-    df[df[label] == 0].sample(int(class_size)),
-    df[df[label] == 1]])'''
-
-    #df_new = df
-    #print (df[label].value_counts())
-
-
-
-
-
-
-
     train_text, temp_text, train_labels, temp_labels = train_test_split(df[text], df[label],test_size=0.3, stratify=df[label]) 
     
     #train_text, temp_text, train_labels, temp_labels = train_test_split(df, df[label], test_size=0.3, stratify=df[label]) 
@@ -161,39 +124,6 @@ def datasplit(label, feat):
     print (train_labels.value_counts())
     print (val_labels.value_counts())
     print (test_labels.value_counts())
-    
-    
-    
-    
-    '''print ("DOING AUGMENTATION")
-    eda_supplement.prepare4eda(label, train_text)
-    print ("here")
-    
-    new_df = eda_supplement.convert_auged(train_text)
-    
-    print (train_text.groupby(by=['AUTHOR_OR', 'FAMILY_OR']))
-    
-    train_text = df[text]
-    train_labels = df[label]
-    
-    print ("DATA SIZE AFTER AUGMENTATION")
-    print (train_text.shape)
-    print (train_labels.shape)
-    
-    
-    
-    val_text = val_text[text]
-    test_text = test_text[text]
-    
-    print ("VAL AND TEST SIZE")
-    print (val_text.shape)
-    print (test_text.shape)'''
-   
-    '''class_sample_count = np.array([len(np.where(train_labels == t)[0]) for t in np.unique(train_labels)])
-    weight = 1. / class_sample_count
-    samples_weight = np.array([weight[t] for t in train_labels])
-    samples_weight = torch.from_numpy(samples_weight)
-    print (samples_weight)'''
     
     return train_text, train_labels, val_text, val_labels, test_text, test_labels, df
 
@@ -239,14 +169,13 @@ def get_data(label, split_type, df, train_text, train_labels, val_text, val_labe
     print (test_text.shape)
 
     #model_name = 'digitalepidemiologylab/covid-twitter-bert'
-    #model_name = 'digitalepidemiologylab/covid-twitter-bert-v2'
-    model_name = 'bert-large-uncased'
+    model_name = 'digitalepidemiologylab/covid-twitter-bert-v2'
+    #model_name = 'bert-large-uncased'
 
 
-    #bert = AutoModel.from_pretrained('digitalepidemiologylab/covid-twitter-bert')
     
-    #bert = AutoModel.from_pretrained('digitalepidemiologylab/covid-twitter-bert-v2')
-    bert = AutoModel.from_pretrained('bert-large-uncased')
+    bert = AutoModel.from_pretrained('digitalepidemiologylab/covid-twitter-bert-v2')
+    
 
     # Load the BERT tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -361,10 +290,9 @@ class BERT_Arch(nn.Module):
         # dense layer 2 (Output layer)
         self.fc2 = nn.Linear(512,1)
 
-        #softmax activation function
-        #self.softmax = nn.LogSoftmax(dim=1)
+       
         self.sigmoid = nn.Sigmoid()
-        #self.tanh = nn.Tanh()
+        
 
     #define the forward pass
     def forward(self, sent_id, mask):
@@ -380,18 +308,12 @@ class BERT_Arch(nn.Module):
 
         # output layer
         x = self.fc2(x)
-        
-
-        # apply softmax activation
-        #x = self.softmax(x)
-        #x = self.sigmoid(x)
-        #x = self.tanh(x)
-        #print (x)
+       
         return x
   
 
 
-    # function to train the model
+    
 def train(model, optimizer, train_dataloader, cross_entropy):
     model.train()
     total_loss, total_accuracy = 0, 0
@@ -430,8 +352,6 @@ def train(model, optimizer, train_dataloader, cross_entropy):
         
         # compute the loss between actual and predicted values
         loss = cross_entropy(preds, labels)
-        #loss = loss_fn(preds, labels)
-        #loss = criterion(preds, labels)
 
         
         
@@ -525,64 +445,6 @@ def evaluate(model, val_dataloader, cross_entropy):
     return avg_loss, total_preds
 
 
-def T_scaling(logits, args):
-    temperature = args.get('temperature', None)
-    return torch.div(logits, temperature)
-
-
-def Temp_scaling(val_dataloader,net, label):
-    
-    import torch.optim as optim
-    
-    temperature = nn.Parameter(torch.ones(1).cuda())
-    args = {'temperature': temperature}
-    criterion = nn.CrossEntropyLoss()
-
-    # Removing strong_wolfe line search results in jump after 50 epochs
-    optimizer = optim.LBFGS([temperature], lr=0.001, max_iter=10000, line_search_fn='strong_wolfe')
-
-    logits_list = []
-    labels_list = []
-    temps = []
-    losses = []
-
-    for i, data in enumerate(tqdm(val_dataloader, 0)):
-        sent, mask, labels = data[0].to(device), data[1].to(device),  data[2].to(device)
-
-        net.eval()
-        with torch.no_grad():
-            logits_list.append(net(sent, mask))
-            labels_list.append(labels)
-
-    # Create tensors
-    logits_list = torch.cat(logits_list).to(device)
-    labels_list = torch.cat(labels_list).to(device)
-
-    def _eval():
-        loss = criterion(T_scaling(logits_list, args), labels_list)
-        loss.backward()
-        temps.append(temperature.item())
-        #print (loss)
-        #print (type(loss))
-        #print (loss.cpu().detach().numpy())
-        losses.append(loss.cpu().detach().numpy())
-        return loss
-
-
-    optimizer.step(_eval)
-
-    print('Final T_scaling factor: {:.2f}'.format(temperature.item()))
-
-    plt.subplot(121)
-    plt.plot(list(range(len(temps))), temps)
-
-    plt.subplot(122)
-    plt.plot(list(range(len(losses))), losses)
-    plt.show()
-    plt.savefig("/home/adhiman/SAR-z/classifier/ct_bert/"+str(label)+"_temp_scale.pdf", bbox_inches='tight')
-    plt.close()
-    
-    return temperature
 
 
 def run(bert, label, train_dataloader, val_dataloader, train_labels, enc_num, save_path):
@@ -618,27 +480,8 @@ def run(bert, label, train_dataloader, val_dataloader, train_labels, enc_num, sa
         if param.requires_grad == True:
             print('I am not frozen: {}'.format(name))
         
-    '''modules = [L1bb.embeddings, *L1bb.encoder.layer[:5]] #Replace 5 by what you want
-    for module in mdoules:
-        for param in module.parameters():
-            param.requires_grad = False'''
+
     
-    '''for param in bert.parameters():
-        param.requires_grad = False ###False for keeping the weights of the pre-trained encoder frozen and optimizing only the weights of the head layers. '''
-
-
-
-    #loss_fn = nn.BCELoss()
-
-    #device = torch.device("cuda:1,3" if torch.cuda.is_available() else "cpu")
-    print('Using device:', device)
-    print()
-
-    print(torch.cuda.current_device())
-
-    #torch.cuda.set_device(1) 
-    #print(torch.cuda.current_device())
-
     
     #batch_size
     # pass the pre-trained BERT to our define architecture
@@ -658,55 +501,25 @@ def run(bert, label, train_dataloader, val_dataloader, train_labels, enc_num, sa
     optimizer = AdamW(model.parameters(),
                       lr = my_lr, eps= 1e-08)          # learning rate
 
-    #scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
+   
 
 
     scheduler = ReduceLROnPlateau(optimizer, 'min', patience= 4, factor=0.01, verbose=True)
 
 
-
-
-
-
     #compute the class weights
     #class_weights = compute_class_weight('balanced', np.unique(train_labels), train_labels)
 
-
-
-
-
-    class_weights = compute_class_weight(
-                                            class_weight = "balanced",
-                                            classes = np.unique(train_labels),
-                                            y = train_labels                                                    
-                                        )
-    #class_weights = dict(zip(np.unique(train_labels), class_weights)),
-    #class_weights
-
-
-
-    #class_weights = [0.52226277, 3.54477612]
-    print("Class Weights:",class_weights)
-
-    # converting list of class weights to a tensor
-    weights= torch.tensor(class_weights,dtype=torch.float)
-
-    # push to GPU
-    weights = weights.to(device)
     if label == 'AUTHOR_OR':
         pos_wt = torch.tensor([2.86])
     if label == 'FAMILY_OR':
         pos_wt = torch.tensor([5.12])
     if label =='ABT_FAMILY':
         pos_wt = torch.tensor([2.82])
-        
-    #pos_wt = torch.FloatTensor([weights[1]]*32)
+       
     pos_wt = pos_wt.to(device)
 
-    # define the loss function
-    #cross_entropy  = nn.NLLLoss(weight=weights) 
-    #cross_entropy  = nn.BCELoss(weight=weights) 
-    #cross_entropy  = nn.BCEWithLogitsLoss(pos_weight=weights[1])  #pos_weight=weights
+   
     cross_entropy  = nn.BCEWithLogitsLoss(pos_weight=pos_wt) 
     
     # number of training epochs
@@ -775,7 +588,7 @@ def test(model, test_seq, test_mask, test_y, train_losses, valid_losses, label, 
     
     
     #load weights of best model
-    #path = '/home/adhiman/SAR-z/classifier/ct_bert/saved_weights_ctbertdnn_2.pt'
+    #path = 'ct_bert/saved_weights_ctbertdnn_2.pt'
     #model.load_state_dict(torch.load(path))
 
     test_seq = test_seq.to(device)
@@ -788,12 +601,7 @@ def test(model, test_seq, test_mask, test_y, train_losses, valid_losses, label, 
     with torch.no_grad():
         preds = model(test_seq, test_mask)
         preds = preds.detach().cpu().numpy()
-    
-    '''if tempr!='x':
-        pred = T_scaling(preds, tempr)'''
 
-    #print (preds)
-    #print (test_y)
     
     pred_sigmoid = torch.sigmoid(torch.from_numpy(preds))
     #pred_tanh = torch.tanh(torch.from_numpy(preds))
@@ -829,46 +637,6 @@ def test(model, test_seq, test_mask, test_y, train_losses, valid_losses, label, 
         f1 = f1_score(test_y, pred_class, average ='macro')
         return pred_sigmoid,  torch.from_numpy(preds), val_th, f1
         
-        
-        
-        
-        '''lines = report.split('\n')
-        textfile = open("/home/adhiman/SAR-z/classifier/ct_bert/results_sigmd_thshld.txt", "a")
-        textfile.write("===================================================================================================================" + "\n")
-        #textfile.write(str(model)+'\n')
-
-        textfile.write("label: "+ str(label) + "\n")
-        textfile.write("threshold: "+ str(th) + "\n")
-        textfile.write("train_index1: "+ str(train_index[0]) + "\n")
-        textfile.write("test_index1: "+ str(test_index[0]) + "\n")
-        textfile.write("train_index2: "+ str(train_index2[0]) + "\n")
-        textfile.write("test_index2: "+ str(test_index2[0]) + "\n")
-        textfile.write("Using encoders: "+ str(enc_num) + "\n")
-        textfile.write("class0: "+ str(df[label].value_counts()) + "\n")
-
-        textfile.write("lr: "+ str(my_lr) + "\n")
-        textfile.write("epochs: "+str(epochs)+"\n")
-        textfile.write("Bacth size: "+str(batch_size)+"\n")
-        for element in lines:
-            textfile.write(element + "\n")
-        textfile.write("accuracy: "+str(acc_score)+"\n")
-        #textfile.write("Temperature: "+str(tempr.item())+"\n")
-        textfile.close()
-
-
-        plt.plot(train_losses, label="Training loss")
-        plt.plot(valid_losses, label="Validation loss")
-        plt.legend()
-        plt.title("Losses")
-        plt.savefig("/home/adhiman/SAR-z/classifier/ct_bert/loss_sig_"+str(label)+".pdf", bbox_inches='tight')
-        plt.close()'''
-        
-    #classification_report_csv(report)
-
-    #del model
-    #del preds
-    #torch.cuda.empty_cache() 
-
     
 
 def data_fold(X, Y, label, feat,train_index, test_index, sk):
@@ -937,13 +705,13 @@ def main2(label,enc_num, run_num, batch_size, feat):
             bert,train_dataloader, val_dataloader, test_seq, test_mask,test_y, val_seq, val_mask, val_y, train_labels, df  = get_data(label, 'split_type', df0, train_text, train_labels, val_text, val_labels, test_text, test_labels, batch_size, feat)
             
             if label =='AUTHOR_OR':
-                save_path ='/disks/sdb/adhiman/SAR-z/ct_bert/testing weight/cross_val_ctbert/saved_weights_ctbertdnn_auth_test'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.pt'
+                save_path ='saved_weights_ctbertdnn_auth_test'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.pt'
             if label =='FAMILY_OR':
-                save_path ='/disks/sdb/adhiman/SAR-z/ct_bert/testing weight/cross_val_ctbert/saved_weights_ctbertdnn_fam_test'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.pt'
+                save_path ='saved_weights_ctbertdnn_fam_test'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.pt'
             if label =='ABT_FAMILY':
-                save_path ='/disks/sdb/adhiman/SAR-z/ct_bert/testing weight/cross_val_ctbert/saved_weights_ctbertdnn_abt_fam_test'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.pt'
+                save_path ='saved_weights_ctbertdnn_abt_fam_test'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.pt'
 
-            print (save_path)
+            
             model, train_losses, valid_losses, epch, lr = run(bert, label, train_dataloader, val_dataloader, train_labels, enc_num, save_path)
             
             val_preds_sig, val_preds, val_th, val_f1 = test(model, val_seq, val_mask, val_y, train_losses, valid_losses,label, df, epch, lr,batch_size, enc_num, train_index,test_index,  train_index2, test_index2, val =True, val_th = 0 )
@@ -952,17 +720,12 @@ def main2(label,enc_num, run_num, batch_size, feat):
             df_val['Sig_Predictions'] = val_preds_sig
             df_val['Predictions'] = val_preds
             if label =='AUTHOR_OR': 
-                df_val.to_csv('/disks/sdb/adhiman/SAR-z/ct_bert/split_data/ct_bert/author_val2/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
+                df_val.to_csv('/author_val2/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
             if label =='FAMILY_OR':
-                df_val.to_csv('/disks/sdb/adhiman/SAR-z/ct_bert/split_data/ct_bert/family_val2/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
+                df_val.to_csv('/family_val2/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
             if label =='ABT_FAMILY':
-                df_val.to_csv('/disks/sdb/adhiman/SAR-z/ct_bert/split_data/ct_bert/abt_family_val/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
-            '''if val_f1>f_val_f1:
-                print ("updating data \n old f1"+str(f_val_f1)+"\t new f1 "+str(val_f1))
-                out_file.write("updating data \n old f1"+str(f_val_f1)+"\t new f1 "+str(val_f1))
-                f_save_path = save_path
-                f_val_f1 = val_f1
-                #tempr = Temp_scaling(val_dataloader,model, label)'''
+                df_val.to_csv('/abt_family_val/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
+         
 
             test_preds_sig, test_preds, test_th, test_f1 = test(model, test_seq, test_mask, test_y, train_losses, valid_losses,label, df, epch, lr,batch_size, enc_num, train_index,test_index,  train_index2, test_index2,  val =False, val_th =val_th  ) #, tempr
 
@@ -970,11 +733,11 @@ def main2(label,enc_num, run_num, batch_size, feat):
             df_temp['Predictions'] = test_preds
             df_temp['Sig_Predictions'] = test_preds_sig
             if label =='AUTHOR_OR': 
-                df_temp.to_csv('/disks/sdb/adhiman/SAR-z/ct_bert/split_data/ct_bert/author_test2/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
+                df_temp.to_csv('/author_test2/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
             if label =='FAMILY_OR':
-                df_temp.to_csv('/disks/sdb/adhiman/SAR-z/ct_bert/split_data/ct_bert/family_test2/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
+                df_temp.to_csv('/family_test2/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
             if label =='ABT_FAMILY':
-                df_val.to_csv('/disks/sdb/adhiman/SAR-z/ct_bert/split_data/ct_bert/abt_family_test/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
+                df_val.to_csv('/abt_family_test/preds_'+str(test_index[0])+'_'+str(train_index2[0])+'_'+str(test_index2[0])+'.csv', index =None)
 
             out_file.write(str(val_th)+"\n")
             out_file.flush()
@@ -993,8 +756,6 @@ def main(label,enc_num, run_num, batch_size, feat):
         save_path ='/home/adhiman/SAR-z/classifier/ct_bert/testing weight/cross_val_ctbert/saved_weights_ctbertdnn_abt_fam_test'+str(run_num)+'.pt'
     print (save_path)
     model, train_losses, valid_losses, epch, lr = run(bert, label, train_dataloader, val_dataloader, train_labels, enc_num, save_path)
-
-    #tempr = Temp_scaling(val_dataloader,model, label)
 
     test(model, test_seq, test_mask, test_y, train_losses, valid_losses,label, df, epch, lr,batch_size, enc_num, 0,0,0,0 ) #, tempr
 
