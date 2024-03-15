de4f28e1342b87fc3c2d6dc0a5e811fab37fbcf0
Aarzoo Dhiman <34305704+AarzooDhiman@users.noreply.github.com>
2024-03-15 12:33:04 +0000

    Update run_ct_bert.sh

--- classifier/run_ct_bert.sh
+++ classifier/run_ct_bert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#This code is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License ([CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/)).
 
 
 for i in 5
