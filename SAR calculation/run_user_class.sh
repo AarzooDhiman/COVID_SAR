5bf2c0cfa009d58d9282a1cb7e8c762ffe807924
Aarzoo Dhiman <34305704+AarzooDhiman@users.noreply.github.com>
2024-03-15 12:30:06 +0000

    Update run_user_class.sh

--- SAR calculation/run_user_class.sh
+++ SAR calculation/run_user_class.sh
@@ -1,1 +1,3 @@
+#This code is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License ([CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/)).
+
 python user_class.py 1 '../data/user_scores/' '../user_labels/'
