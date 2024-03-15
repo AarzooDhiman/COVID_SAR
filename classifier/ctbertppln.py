7f10907f9c8e2b1d72322afc1cc9d0e5f4bef072
Aarzoo Dhiman <34305704+AarzooDhiman@users.noreply.github.com>
2024-03-15 12:32:32 +0000

    Update ctbertppln.py

--- classifier/ctbertppln.py
+++ classifier/ctbertppln.py
@@ -1,3 +1,5 @@
+#This code is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License ([CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/)).
+
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"
 
