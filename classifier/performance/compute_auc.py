ec35644de3252394589be054293260a425c0aea3
Aarzoo Dhiman <34305704+AarzooDhiman@users.noreply.github.com>
2024-03-15 12:31:40 +0000

    Update compute_auc.py

--- classifier/performance/compute_auc.py
+++ classifier/performance/compute_auc.py
@@ -1,3 +1,5 @@
+# This code is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License ([CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/)).
+
 import pandas as pd
 import glob
 from sklearn import metrics
